5860fcc63fb59ddb83f72e5b7da2679edef3c51e
Muzammil Malik <abdmalik1593@gmail.com>
2024-02-06 16:20:21 +0530

    forms added for new resume

--- frontend/src/pages/GenerateNewResume.jsx
+++ frontend/src/pages/GenerateNewResume.jsx
@@ -21,6 +21,64 @@ const GenerateNewResume = () => {
           a tailored resume just for you.
         </p>
       </div>
+      <div className="w-full px-4 py-3 border-b-gray-200 border-b flex ">
+        <div className="left w-[50%]">
+          <form className="flex flex-col w-[80%] space-y-3">
+            <label className="text-base my-1 px-1 font-inter">Name</label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input font-inter px-3 py-2 mb-10 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+            />
+            <label className="text-base my-1 px-1 font-inter">
+              Short Description
+            </label>
+            <input
+              type="text"
+              placeholder="Short Description"
+              className="input font-inter px-3 py-2 mb-3 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+            />
+            <label className="text-base my-1 px-1 font-inter">
+              Education details
+            </label>
+            <textarea
+              rows="8"
+              cols="50"
+              className="input resize-none font-inter px-3 py-2 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+              placeholder="Education Details"
+            />
+            <label className="text-base my-1 px-1 font-inter">
+              Work Experience
+            </label>
+            <textarea
+              rows="8"
+              cols="50"
+              className="input resize-none font-inter px-3 py-2 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+              placeholder="Work Experience"
+            />
+            <label className="text-base my-1 px-1 font-inter">
+              Achievements
+            </label>
+            <textarea
+              cols="50"
+              rows="8"
+              className="input resize-none font-inter px-3 py-2 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+              placeholder="Achievements"
+            />
+            <label className="text-base my-1 px-1 font-inter">
+              Job Description
+            </label>
+            <textarea
+              cols="50"
+              rows="8"
+              className="input resize-none font-inter px-3 py-2 text-[#3F292B] outline-none border rounded-lg w-[80%]"
+              type="text"
+              placeholder="Job description"
+            />
+          </form>
+        </div>
+        <div className="right"></div>
+      </div>
     </div>
   );
 };
