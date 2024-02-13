02302c9743593c8fb9bf7bb06695468c5442120b
manish jha <mj1898520@gmail>
2024-02-13 13:31:39 +0530

    file added

--- frontend/src/pages/NewSupport.jsx
+++ frontend/src/pages/NewSupport.jsx
@@ -2,64 +2,81 @@ import React, { useState } from "react";
 import Dropdown from "./Dropdown-items";
 import Dropdown2 from "./Dropdown2";
 import { Input } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 const NewSupport = () => {
   return (
-    <div className="new-support-container flex flex-col items-center">
-      <div className="pragaraph1">
-        <p className="pt-8">
+    <>
+      <div className="w-full px-4 py-3 border-b-gray-200 border-b ">
+        <div className="flex gap-1 items-center text-black">
+          <Link to={`/dashboard`}>
+            <IoIosArrowRoundBack />
+          </Link>
+          <Link to={`/dashboard`}>
+            <p className="text-sm font-inter text-black">Back</p>
+          </Link>
+        </div>
+        <h1 className=" py-4 text-3xl text-black font-inter font-bold pb-0">
+          Create New Support Request
+        </h1>
+        <p className="pt-3">
           Generate new support request. We will answer as soon as possible.
         </p>
-        <p className="font-inter font-medium text-4xl pt-8">
-          Create New Support Request
-        </p>
       </div>
+      <div className="supportContainer px-4 py-3 border-b-gray-200 border-b">
+        <div className="new-support-container flex flex-col items-center">
+          <div className="pragaraph1"></div>
 
-      <div className="form-part flex flex-row md1:ml-9  md:flex-row gap-12 w-full justify-center items-center sm:flex-col pt-5">
-        <div className="left-form md:w-1/2">
-          {" "}
-          {/* Set width to 50% on desktop */}
-          <p className=" font-inter">Support Category</p>
-          <Dropdown />
+          <div className="form-part flex flex-row md1:ml-9  md:flex-row gap-12 w-full justify-center items-center sm:flex-col pt-5">
+            <div className="left-form md:w-1/2">
+              {/* Set width to 50% on desktop */}
+              <p className=" font-inter pb-3">Support Category</p>
+              <Dropdown />
+            </div>
+            <div className="right-form md:w-1/2">
+              {/* Set width to 50% on desktop */}
+              <p className=" font-inter pb-3"> Support Priority</p>
+              <Dropdown2 />
+            </div>
+          </div>
+
+          <div className="subject w-full flex flex-col items-center pt-6">
+            <p className=" font-inter">Subject</p>
+            <div className="input-box-centre">
+              <Input
+                placeholder="Please enter subject of the support request"
+                size="sm"
+                style={{
+                  width: "25rem",
+                  marginTop: "1rem",
+                  borderRadius: "1rem",
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="max-w-md w-full p-4 rounded-md flex flex-col items-center">
+            <label
+              htmlFor="message"
+              className=" font-inter block text-sm font-medium text-gray-700"
+            >
+              Your Message:
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
+            ></textarea>
+
+            <button className="text-base text-white w-36 outline-none rounded-3xl py-2 px-2 bg-lteal shadow transition duration-500 ease-in-out hover:shadow-lg hover:-translate-y-1.5 focus:outline-none focus:shadow-outline-blue active:shadow-none lg:hover:shadow lg:hover:-translate-y-0 mt-14 ">
+              send
+            </button>
+          </div>
         </div>
-        <div className="right-form md:w-1/2">
-          {" "}
-          {/* Set width to 50% on desktop */}
-          <p className=" font-inter"> Support Priority</p>
-          <Dropdown2 />
-        </div>
       </div>
-
-      <div className="subject w-full flex flex-col items-center pt-6">
-        <p className=" font-inter">Subject</p>
-        <div className="input-box-centre">
-          <Input
-            placeholder="Please enter subject of the support request"
-            size="sm"
-            style={{ width: "25rem", marginTop: "1rem", borderRadius: "1rem" }}
-          />
-        </div>
-      </div>
-
-      <div className="max-w-md w-full p-4 rounded-md flex flex-col items-center">
-        <label
-          htmlFor="message"
-          className=" font-inter block text-sm font-medium text-gray-700"
-        >
-          Your Message:
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          rows="4"
-          className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
-        ></textarea>
-
-        <button className="text-base text-white w-36 outline-none rounded-3xl py-2 px-2 bg-lteal shadow transition duration-500 ease-in-out hover:shadow-lg hover:-translate-y-1.5 focus:outline-none focus:shadow-outline-blue active:shadow-none lg:hover:shadow lg:hover:-translate-y-0 mt-14 ">
-          send
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/SupportPage.jsx
+++ frontend/src/pages/SupportPage.jsx
@@ -1,30 +1,40 @@
 import React from "react";
+import { IoIosArrowBack, IoIosArrowRoundBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 const Supportpage = () => {
   return (
     <>
-      <div className="parent-support pl-3 pt  border-b-gray-200 border-b py-11">
-        <p className=" font-inter"> Back to dashbord</p>
-        <p className=" Requests text-3xl font-inter"> Support Requests</p>
+      <div className="w-full px-4 py-3 border-b-gray-200 border-b ">
+        <div className="flex gap-1 items-center text-black">
+          <Link to={`/dashboard`}>
+            <IoIosArrowRoundBack />
+          </Link>
+          <Link to={`/dashboard`}>
+            <p className="text-sm font-inter text-black">Back to dashboard</p>
+          </Link>
+        </div>
+        <h1 className=" py-4 text-3xl text-black font-inter font-bold pb-0">
+          Support Page
+        </h1>
       </div>
+      <div className="container px-4 py-3">
+        <Link to="new-support-request">
+          <button className="text-base font-inter text-white w-[18rem] outline-none rounded-3xl py-2 px-2 bg-lteal shadow transition duration-500 ease-in-out hover:shadow-lg hover:-translate-y-1.5 focus:outline-none focus:shadow-outline-blue active:shadow-none lg:hover:shadow lg:hover:-translate-y-0 ">
+            Create new Support Request
+          </button>
+        </Link>
 
-      <Link to="new-support-request">
-        <button className="text-base font-inter text-white w-[18rem] outline-none rounded-3xl py-2 px-2 bg-lteal shadow transition duration-500 ease-in-out hover:shadow-lg hover:-translate-y-1.5 focus:outline-none focus:shadow-outline-blue active:shadow-none lg:hover:shadow lg:hover:-translate-y-0 mt-[2rem] ml-2 ">
-          Create new Support Request
-        </button>
-      </Link>
-
-      <div className=" pl-[3rem] w-full font-inter border-b-gray-200 border-b   border-t-gray-200 border-t my-8 sm:overflow-x-auto py-[2rem] xl: overflow-x-auto">
-        <div className="different-id flex gap-1 whitespace-nowrap font-inter">
-          <p className="min-w-[8rem]">TICKET ID</p>
-          <p className="min-w-[8rem]">STATUS</p>
-          <p className="min-w-[8rem]">CATEGORY</p>
-          <p className="min-w-[8rem]">SUBJECT</p>
-          <p className="min-w-[8rem]">SUBJECT</p>
-          <p className="min-w-[8rem]">PRIORITY</p>
-          <p className="min-w-[8rem]">CREATED AT</p>
-          <p className="min-w-[8rem]">LAST UPDATED</p>
-          <p className="min-w-[8rem]">ACTIONS</p>
+        <div className=" px-4 py-3 mt-4 w-full font-inter border-b-gray-200 border-b border-t-gray-200 border-t sm:overflow-x-auto xl: overflow-x-auto">
+          <div className="different-id flex whitespace-nowrap font-inter">
+            <p className="p min-w-36">Ticket Id</p>
+            <p className="p min-w-36">Status</p>
+            <p className="p min-w-36">Category</p>
+            <p className="p min-w-36">Subject</p>
+            <p className="p min-w-36">Priority</p>
+            <p className="p min-w-36">Created At</p>
+            <p className="p min-w-36">Last Updated</p>
+            <p className="p min-w-36">Actions</p>
+          </div>
         </div>
       </div>
     </>
